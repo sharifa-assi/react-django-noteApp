d9b03ee5fec94f669558e792f7218546f044eab2
sharifa-assi <sharifaalassi2001@gmail.com>
2022-11-08 14:36:15 +0200

    setting up react project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import NotesList, { } from './pages/NotesList';
+
+function App() {
+  return (
+    <div>
+<Header/>
+<NotesList/>
+    </div>
+  );
+}
+
+export default App;
